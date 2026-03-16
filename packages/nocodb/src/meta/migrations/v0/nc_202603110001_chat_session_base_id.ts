d6545ff127e29f7256da750c4b0d8d13d30ea8b7
DarkPhoenix2704 <anbarasun123@gmail.com>
2026-03-16 16:03:12 +0000

    fix: merge migrations

--- packages/nocodb/src/meta/migrations/v0/nc_202603110001_chat_session_base_id.ts
+++ packages/nocodb/src/meta/migrations/v0/nc_202603110001_chat_session_base_id.ts
@@ -1,19 +1,17 @@
 import type { Knex } from 'knex';
 import { MetaTable } from '~/utils/globals';
-
+import {
+  down as revertMessageTable,
+  up as updateMessageTable,
+} from '~/meta/migrations/chat-messages/nc_002_base_id';
 const up = async (knex: Knex) => {
-  await knex(MetaTable.CHAT_MESSAGES).del();
   await knex(MetaTable.CHAT_SESSIONS).del();
 
   await knex.schema.alterTable(MetaTable.CHAT_SESSIONS, (table) => {
     table.string('base_id', 20);
   });
 
-  await knex.schema.alterTable(MetaTable.CHAT_MESSAGES, (table) => {
-    table.string('base_id', 20);
-    table.string('bt_span_id', 100);
-    table.text('files');
-  });
+  await updateMessageTable(knex);
 
   await knex.schema.alterTable(MetaTable.FILE_REFERENCES, (table) => {
     table.string('fk_session_id', 20);
@@ -26,11 +24,7 @@ const down = async (knex: Knex) => {
     table.dropColumn('base_id');
   });
 
-  await knex.schema.alterTable(MetaTable.CHAT_MESSAGES, (table) => {
-    table.dropColumn('base_id');
-    table.dropColumn('bt_span_id');
-    table.dropColumn('files');
-  });
+  await revertMessageTable(knex);
 
   await knex.schema.alterTable(MetaTable.FILE_REFERENCES, (table) => {
     table.dropIndex(['base_id', 'fk_session_id'], 'nc_fr_session_idx');
